64068c0350ce95d5819508e9bb9e47e9f70b591d
maozi <xmiller@vip.qq.com>
2016-11-30 13:19:33 +0800

    fix memory buffer

--- dqn.py
+++ dqn.py
@@ -51,7 +51,7 @@ class imgQueue:
         self.__queue.append(img)
 
     def addInfo(self, info):
-        if len(self.__info) >= replayMemory:
+        if len(self.__info) - imgChannel >= replayMemory:
            self.__info.popleft()
         self.__info.append(info)
 
